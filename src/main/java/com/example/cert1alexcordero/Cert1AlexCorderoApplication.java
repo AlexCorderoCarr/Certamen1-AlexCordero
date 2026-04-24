b1e1bed071e506ce3fccfe584949fad478df1cf3
Alex Cordero <alexcordero9mayo@gmail.com>
2026-04-24 15:30:46 -0400

    Verificacion Github remoto, primeras package hechas

---
+++ src/main/java/com/example/cert1alexcordero/Cert1AlexCorderoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cert1alexcordero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cert1AlexCorderoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cert1AlexCorderoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cert1alexcordero/Cert1AlexCorderoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cert1alexcordero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cert1AlexCorderoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
